09c07dc30d63794ed6b320fe49ffaee3e9e52b0e
holotape <7445231+holotape@users.noreply.github.com>
2024-03-20 22:15:08 -0400

    Added image icons. Issue with rechecking

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pytz
 import requests
 from icalendar import Calendar
 from PIL import Image, ImageDraw, ImageFont
+import RPi.GPIO as GPIO
 from waveshare_OLED import OLED_1in5_rgb
 
 picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
@@ -56,15 +57,16 @@ def get_current_event_status(file_path_or_url):
         if event_start <= now <= event_end:
             summary = str(event.get('summary'))
             if "Focus" in summary:
-                return "Do Not Disturb  - ", "red"
+                return "Do Not Disturb  - ", "red", "dnd-128.png"
             elif "Lunch" in summary:
-                return "Out to Lunch  -", "yellow"
+                return "Out to Lunch  -", "yellow", "time-4-128.png"
             elif "WFH" in summary:
-                return "Working Elsewhere  -", "blue"
+                return "Working Elsewhere  -", "blue", "house-128.png"
             else:
-                return "Do Not Disturb  -", "red"  # Default for any other event
+                return "Do Not Disturb  -", "red", "dnd-128.png"  # Default for any other event
 
-    return "", None  # No event - blank screen
+    return "", None, None  # No event - blank screen
+
 
 def display_status(status, color):
     if status:  # Only display if there is a status
@@ -83,24 +85,33 @@ color_map = {
     "blue": (10, 10, 255),
 }
 
-def display_scrolling_status(status, color, scroll_delay=0.001):
+def display_scrolling_status(status, color, icon_filename, scroll_delay=0.001):
     if not status:
         oled.clear()
         return
 
+    icon_path = os.path.join(picdir, 'icons', icon_filename) if icon_filename else None
+    icon = Image.open(icon_path) if icon_path else None
+
     font = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    text_width, text_height = font.getsize(status)
+    
+    bbox = font.getbbox(status)
+    text_width, text_height = bbox[2], bbox[3]
     image_width, image_height = oled.width, oled.height
 
     start_offset = 300
     text_x = image_width - start_offset  # Start text on the right side of the screen
     text_y = image_height - text_height  # Position text at the bottom of the screen
 
-    restart_x = image_width - font.getsize("-")[0]  # when to restart
+    bbox_restart = font.getbbox("-")
+    restart_x = image_width - bbox_restart[2]  # when to restart
 
     while True:
         image = Image.new('RGB', (image_width, image_height), 'BLACK')
         draw = ImageDraw.Draw(image)
+
+        if icon:
+            image.paste(icon, (16, 0)) # image display coordinates
 
         text_color = color_map.get(color, 'WHITE')
         draw.text((text_x, text_y), status, font=font, fill=text_color)
@@ -117,10 +128,11 @@ def display_scrolling_status(status, color, scroll_delay=0.001):
 
 if __name__ == '__main__':
     while True:
+        print("Checking for new meetings...")
         file_path_or_url = read_ics_link()
-        status, color = get_current_event_status(file_path_or_url)
+        status, color, icon_filename = get_current_event_status(file_path_or_url)
         long_status = (status + "  ") * 10
-        display_scrolling_status(long_status, color)
+        display_scrolling_status(long_status, color, icon_filename, scroll_delay=0.001)
         time.sleep(120)  # Wait 2 minutes before next check
 
 def wrap_text(text, font, max_width):
@@ -163,9 +175,10 @@ def display_status(status, color=None):
         oled.clear()  # Clear the screen if there's no status
 
 
-if __name__ == '__main__':
-    while True:
-        file_path_or_url = read_ics_link()
-        status, color = get_current_event_status(file_path_or_url)
-        display_status(status, color)
-        time.sleep(300)  # Wait 5 minutes before next check
+# if __name__ == '__main__':
+#      while True:
+#          print("Checking for new meetings2...")
+#          file_path_or_url = read_ics_link()
+#          status, color, icon_filename = get_current_event_status(file_path_or_url)
+#          display_status(status, color)
+#          time.sleep(300)  # Wait 5 minutes before next check
